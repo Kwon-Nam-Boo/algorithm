1c9ae96a6eae3c16472b17ee1ddc7168a224de27
lukas523 <lukas523@naver.com>
2021-02-01 02:06:14 +0900

    BOJ update

---
+++ 00_algorithm/algo_basic/src/rhtn_homework/BOJ_11003_최솟값찾기.java
@@ -0,0 +1,54 @@
+package rhtn_homework;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class BOJ_11003_최솟값찾기 {
+	
+	private static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()); 
+		int L = Integer.parseInt(st.nextToken()); 
+		
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		// 인덱스를 담을 DQ
+		Deque<Integer> dq = new ArrayDeque<>();
+		
+
+		for (int i = 0; i < arr.length; i++) {
+			
+			arr[i] = Integer.parseInt(st.nextToken());
+			
+			// 만약 들어올 값보다 크다면 해당값을 버리는 걸 반복
+			while(!dq.isEmpty() && arr[dq.peekLast()] > arr[i]) {
+				dq.pollLast();
+			}
+			dq.offer(i);
+			
+			// 만약 인덱스가 주어진 범위보다 작은 숫자라면 없애주자
+			if(i-L +1 > dq.peekFirst()) {
+				dq.pollFirst();
+			}
+			
+			sb.append(arr[dq.peekFirst()] +" ");
+		}
+		System.out.println(sb);
+		
+	}
+
+}
--- 00_algorithm/algo_basic/src/rhtn_homework/BOJ_2096_내려가기.java
+++ 00_algorithm/algo_basic/src/rhtn_homework/BOJ_2096_내려가기.java
@@ -3,7 +3,6 @@ package rhtn_homework;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_2096_내려가기 {
@@ -16,6 +15,7 @@ public class BOJ_2096_내려가기 {
 		
 		N = Integer.parseInt(br.readLine());
 		
+		// 메모리 절약을 위해 단 두줄만 사용
 		int[][] map = new int[2][3];
 		int[][] DP = new int[2][3];
 		int[][] DP2 = new int[2][3];
@@ -23,6 +23,7 @@ public class BOJ_2096_내려가기 {
 		int ans = 0;
 		int ans2 = Integer.MAX_VALUE;
 		
+		// 처음 줄 입력 받고 초기화 과정 ...
 		st = new StringTokenizer(br.readLine());
 		for (int c = 0; c < 3; c++) {
 			map[0][c] = Integer.parseInt(st.nextToken());
@@ -30,6 +31,7 @@ public class BOJ_2096_내려가기 {
 			DP2[0][c] = map[0][c];
 		}
 		
+		// 다음줄 입력 및 DP 계산
 		for (int r = 1; r < N; r++) {
 			st = new StringTokenizer(br.readLine());
 			for (int c = 0; c < 3; c++) {
@@ -39,17 +41,16 @@ public class BOJ_2096_내려가기 {
 					DP2[1][c] = Integer.MAX_VALUE;
 				}
 			}
-
+			// DP 작업
 			for (int c = 0; c < 3; c++) {
 				for (int i = -1; i < 2; i++) {
 					if(isIn(1,c+i)) {
 						DP[1][c] = Math.max(DP[1][c], DP[0][c+i] + map[1][c]);
 						DP2[1][c] = Math.min(DP2[1][c], DP2[0][c+i] + map[1][c]);
 					}
-				}
-				
+				}	
 			}
-
+			// 해당 값을 위로 옮기고 밑에 값을 초기화
 			for (int c = 0; c < 3; c++) {
 				map[0][c] = map[1][c]; 
 				DP[0][c] = DP[1][c];
@@ -57,23 +58,19 @@ public class BOJ_2096_내려가기 {
 				DP[1][c] = 0;
 				DP2[1][c] = Integer.MAX_VALUE;
 			}
-
 		}
-//		for (int r = 0; r < DP2.length; r++) {
-//			System.out.println(Arrays.toString(DP[r]));
-//		}
 		
+		// 최대, 최소값 찾기
 		for (int c = 0; c < 3; c++) {
 			ans = Math.max(ans, DP[0][c]);
 			ans2 = Math.min(ans2, DP2[0][c]);
 		}
-	
-
+		
 		System.out.println(ans + " " + ans2);
 	}
 	
 	private static boolean isIn(int r, int c) {
-		return r>=0 && c>=0 && r< 3 && c< 3;
+		return r>=0 && c>=0 && r < 2 && c< 3;
 	}
 
 }
