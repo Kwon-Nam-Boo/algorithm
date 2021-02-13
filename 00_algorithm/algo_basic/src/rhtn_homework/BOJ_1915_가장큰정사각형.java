204c9116b85fdde36e07b2378255dd14434a912f
lukas523 <lukas523@naver.com>
2021-02-13 16:06:57 +0900

    BOJ update_2021_02_13

---
+++ 00_algorithm/algo_basic/src/rhtn_homework/BOJ_1915_가장큰정사각형.java
@@ -0,0 +1,46 @@
+package rhtn_homework;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1915_가장큰정사각형 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[][] DP = new int[N][M];
+		int ans = 0;
+		
+		for (int i = 0; i < N; i++) {
+			char[] line = br.readLine().toCharArray();
+			for (int j = 0; j < line.length; j++) {
+				// 만약 i가 0 이면(첫줄) j가 0이면 (맨 왼쪽), DP랑 같다
+				if(i == 0 || j ==0) {
+					DP[i][j] = line[j] - '0';
+					ans = Math.max(ans, DP[i][j]);
+					continue;
+				}
+				if(line[j] == '1') {
+					int min = 0;
+					min = Math.min(DP[i-1][j], DP[i][j-1]);
+					min = Math.min(DP[i-1][j-1], min);
+					DP[i][j] = min +1;
+					ans = Math.max(ans, DP[i][j]);
+				}
+			}
+		}
+//		for (int[] is : DP) {
+//			System.out.println(Arrays.toString(is));
+//		}
+		System.out.println(ans*ans);
+	}
+
+}
