ac4b50faeb5f56a77f4da890a11749b652e3013c
lukas523 <lukas523@naver.com>
2021-04-08 16:04:30 +0900

    BOJ update_2021_04_07

--- 00_algorithm/algo_basic/src/codingTest2/dev_03.java
+++ 00_algorithm/algo_basic/src/codingTest2/dev_03.java
@@ -15,8 +15,10 @@ public class dev_03 {
 	}
 	
 	public static int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+		// 0은 center지만 해당로직에서는 필요없다(무시)
         int[] answer = new int[enroll.length+1];
         
+        // 해쉬 매핑(1부터 시작)
         HashMap<String, Integer> hash = new HashMap<>();
         for (int i = 0; i < enroll.length; i++) {
 			hash.put(enroll[i], i+1);
@@ -25,14 +27,16 @@ public class dev_03 {
         // 해당 사람의 부모값
         int[] parent = new int[enroll.length+1];
         
+        // 부모값 넣어주기(1부터시작)
         for (int i = 0; i < referral.length; i++) {
 			if(referral[i].equals("-")) continue;
 			parent[i+1] = hash.get(referral[i]);
 		}
         //System.out.println(Arrays.toString(parent));
         
-        // seller.length
+        
         for (int i = 0; i < seller.length; i++) {
+        	// 위치, 돈
         	int idx = hash.get(seller[i]);
         	int money = amount[i]*100;
         	
@@ -44,7 +48,9 @@ public class dev_03 {
         	}
         	//System.out.println(Arrays.toString(answer));
 		}
+        
         int[] ans = new int[enroll.length];
+        // center값 제외, 앞으로 당겨주고 출력
         for (int i = 0; i < ans.length; i++) {
 			ans[i] = answer[i+1];
 		}
--- 00_algorithm/algo_basic/src/rhtn_homework/BOJ_17103_골드바흐파티션.java
+++ 00_algorithm/algo_basic/src/rhtn_homework/BOJ_17103_골드바흐파티션.java
@@ -3,13 +3,46 @@ package rhtn_homework;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class BOJ_17103_골드바흐파티션 {
 
+	private static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		ArrayList<Integer> primes = new ArrayList<>();
+		boolean[] check = new boolean[1000001];
+		for (int i = 2; i < check.length; i++) {
+			if(!check[i]) {
+				primes.add(i);
+				for (int j = i+i; j < check.length; j+=i) {
+					check[j] = true;
+				}
+			}
+		}
+	
+		//System.out.println(primes);
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++) {
+			int n = Integer.parseInt(br.readLine());
+			int ans = 0;
+			
+			for (int x : primes){
+				int y = n - x;
+				if(x <= y && !check[y]){
+					ans++;
+				}
+			}
+			sb.append(ans +"\n");
+		}
+		System.out.println(sb);
+		
 	}
+	
+	
 
 }
